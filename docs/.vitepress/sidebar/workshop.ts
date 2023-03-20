4b38d1be114277f3fc054e2b92d3004b2602c0e7
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-03-20 15:20:26 +0800

    move page

--- docs/.vitepress/sidebar/workshop.ts
+++ docs/.vitepress/sidebar/workshop.ts
@@ -13,8 +13,6 @@ export default [
           items: [
             { text: '專案建立', link :'/workshop/project/create' },
             { text: '專案設置', link :'/workshop/project/configure' },
-            // { text: '靜態資源', link :'/workshop/project/resource' },
-            { text: '網站主題', link :'/workshop/project/theme' },
             { text: '版面配置', link :'/workshop/project/layout' },
             { text: '路由配置', link :'/workshop/project/route' },
           ]
@@ -24,7 +22,6 @@ export default [
           collapsible: true,
           items: [
             { text: '建立頁面', link :'/workshop/search-page/01create' },
-            { text: '前端路由', link :'/workshop/search-page/02route' },
             { text: 'AJAX管理', link :'/workshop/search-page/03api' },
             { text: '搜尋區域', link :'/workshop/search-page/04query' },
             { text: '搜尋結果', link :'/workshop/search-page/05result' },
@@ -37,6 +34,7 @@ export default [
           items: [
             { text: '容器部署', link :'/workshop/other/deploy' },
             { text: '常用套件', link :'/workshop/other/packages' },
+            { text: '網站主題', link :'/workshop/other/theme' },
             { text: '渲染函示', link :'/workshop/other/render-function' },
             { text: 'TypeScript', link :'/workshop/other/typescript' },
             { text: '設計模式', link :'/workshop/other/pattern' },
