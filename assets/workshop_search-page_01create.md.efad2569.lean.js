835c0f8b503a6823c7f157f0a97b986d80f02e08
tigernaxoJr <tigernaxoJr@users.noreply.github.com>
2023-03-20 05:38:47 +0000

    deploy: 445585bf8604c78349d22d74a64599e40f29f977

--- assets/workshop_search-page_01create.md.efad2569.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,N as l}from"./chunks/framework.03ba4d3f.js";const A=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"workshop/search-page/01create.md"}'),p={name:"workshop/search-page/01create.md"},o=l("",12),e=[o];function t(c,r,D,F,y,i){return n(),a("div",null,e)}const d=s(p,[["render",t]]);export{A as __pageData,d as default};
