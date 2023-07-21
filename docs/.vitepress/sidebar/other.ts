40b0d7d6320ebe8458efdff63601aa752a8d64df
Peter.Chen <tigernaxo@gmail.com>
2023-07-22 01:04:41 +0800

    other/throttle-debunce

--- docs/.vitepress/sidebar/other.ts
+++ docs/.vitepress/sidebar/other.ts
@@ -3,15 +3,16 @@ export default [
     text: '其他',
     collapsible: true,
     items: [
-      { text: '容器部署', link :'/workshop/other/deploy' },
-      { text: 'WebView2', link :'/workshop/other/webview2' },
-      { text: '網站主題', link :'/workshop/other/theme' },
-      { text: '渲染函示', link :'/workshop/other/render-function' },
-      { text: 'JS雜事', link :'/workshop/other/javascript' },
-      { text: 'TypeScript', link :'/workshop/other/typescript' },
-      { text: '設計模式', link :'/workshop/other/pattern' },
-      { text: '去耦合', link :'/workshop/other/decouple' },
-      { text: '常用套件', link :'/workshop/other/packages' },
-    ]
-  }
-]
+      { text: '容器部署', link: '/other/deploy' },
+      { text: 'WebView2', link: '/other/webview2' },
+      { text: '網站主題', link: '/other/theme' },
+      { text: '渲染函示', link: '/other/render-function' },
+      { text: 'JS雜事', link: '/other/javascript' },
+      { text: 'TypeScript', link: '/other/typescript' },
+      { text: '設計模式', link: '/other/pattern' },
+      { text: '去耦合', link: '/other/decouple' },
+      { text: '常用套件', link: '/other/packages' },
+      { text: '節流與防抖', link: '/other/throttle-debounce' },
+    ],
+  },
+];
