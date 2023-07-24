2df8892f5609673232f9e8044627c9b80ca6327b
tigernaxoJr <tigernaxoJr@users.noreply.github.com>
2023-07-24 09:25:32 +0000

    deploy: 299c64f90cb107d540b76bd2cfb69817aaa8b0d0

---
+++ assets/vue3_route.md.1a76a2b4.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,c as r,U as d}from"./chunks/framework.2939a8e5.js";const b=JSON.parse('{"title":"前端路由","description":"","frontmatter":{},"headers":[],"relativePath":"vue3/route.md","filePath":"vue3/route.md"}'),e={name:"vue3/route.md"},o=d("",7),h=[o];function s(l,n,i,c,p,_){return a(),r("div",null,h)}const f=t(e,[["render",s]]);export{b as __pageData,f as default};
