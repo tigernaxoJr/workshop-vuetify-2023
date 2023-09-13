958af37d01946c247b8a6b4a5eb732e731de3022
tigernaxoJr <tigernaxoJr@users.noreply.github.com>
2023-09-13 15:40:32 +0000

    deploy: b4584ee15095330b38f2248f141fba812713ee71

--- assets/other_sso.md.d09bb656.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c as t,Q as a}from"./chunks/framework.1eafcbb1.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"other/sso.md","filePath":"other/sso.md"}'),o={name:"other/sso.md"},i=a("",5),l=[i];function n(h,c,s,f,d,_){return r(),t("div",null,l)}const p=e(o,[["render",n]]);export{m as __pageData,p as default};
