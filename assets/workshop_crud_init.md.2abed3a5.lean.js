a51071b326a75e50e69b9423f0d21da3d635be4c
tigernaxoJr <tigernaxoJr@users.noreply.github.com>
2023-09-14 15:31:53 +0000

    deploy: d38d044c07e6304159b38462f1b06841bfa533cd

--- assets/workshop_crud_init.md.2abed3a5.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as r}from"./chunks/framework.1eafcbb1.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"workshop/crud/init.md","filePath":"workshop/crud/init.md"}'),o={name:"workshop/crud/init.md"};function a(n,s,c,i,p,d){return e(),r("div")}const f=t(o,[["render",a]]);export{m as __pageData,f as default};
