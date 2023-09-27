74b6ddeeeb34a247da23a11d1f9e970c4804eeb0
tigernaxoJr <tigernaxoJr@users.noreply.github.com>
2023-09-27 17:48:11 +0000

    deploy: 2abfc57843b59723b8b69ef84156b22007593241

---
+++ assets/workshop_other_mvvm.md.8b3d2d3a.lean.js
@@ -0,0 +1,9 @@
+import{_ as a,o as r,c as o,Q as e,k as t}from"./chunks/framework.fb95d6d7.js";const f=JSON.parse('{"title":"MVVM","description":"","frontmatter":{},"headers":[],"relativePath":"workshop/other/mvvm.md","filePath":"workshop/other/mvvm.md"}'),d={name:"workshop/other/mvvm.md"},l=e("",5),n=t("iframe",{height:"500",style:{width:"100%"},scrolling:"no",title:"2023-mvc",src:"https://codepen.io/tigernaxoJr/embed/rNovozz?default-tab=html%2Cresult",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/tigernaxoJr/pen/rNovozz">
+  2023-mvc</a> by tigernaxoJr (<a href="https://codepen.io/tigernaxoJr">@tigernaxoJr</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),i=e("",4),h=t("iframe",{height:"500",style:{width:"100%"},scrolling:"no",title:"Untitled",src:"https://codepen.io/tigernaxoJr/embed/wvRjRQG?default-tab=html%2Cresult",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/tigernaxoJr/pen/wvRjRQG">
+  Untitled</a> by tigernaxoJr (<a href="https://codepen.io/tigernaxoJr">@tigernaxoJr</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),s=e("",12),c=[l,n,i,h,s];function m(p,_,v,b,V,u){return r(),o("div",null,c)}const g=a(d,[["render",m]]);export{f as __pageData,g as default};
