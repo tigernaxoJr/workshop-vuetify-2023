e9fa56b126c52ea46169ee056e9277be178556c6
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-10-10 10:35:36 +0800

    opening

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,14 +5,14 @@ import { fileURLToPath } from 'url';
 import sidebar from './sidebar';
 const GA4_ID = 'G-H2T39417H1';
 export default defineConfig({
-  title: 'Vuetify 2023',
+  title: 'Vuetify BootCamp',
   description: 'Just playing around.',
   base: '',
   // appearance: 'dark',
   cleanUrls: false,
   themeConfig: {
     nav: [
-      { text: '工作坊', link: '/workshop/vue3/sfc' },
+      { text: '工作坊', link: '/workshop/workshop' },
       // { text: '其他', link: '/other/other' },
       // { text: 'Reference', link: '/reference/component' },
       { text: '輸入密鑰', link: '/decrypt' },
